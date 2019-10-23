35f6bfb92e1a98e763af92b8f828aa65902aaac5
gorbachproject <56885903+gorbachproject@users.noreply.github.com>
2019-10-23 20:06:05 +0300

    Add files via upload

---
+++ BotCrypto/Market.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbitrationCrypto
+{
+    public class Market
+    {
+        public string MarketName { get; set; }
+        public string MarketCurrency { get; set; }
+        public string BaseCurrency { get; set; }
+
+        public Market()
+        {
+
+        }
+
+        public Market(string Symbol)
+        {
+            string[] tstr = Symbol.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+            MarketName = Symbol;
+            BaseCurrency = tstr[0];
+            MarketCurrency = tstr[1];
+        }
+
+        public Market(string Base, string Market)
+        {
+            this.BaseCurrency = Base.ToUpper();
+            this.MarketCurrency = Market.ToUpper();
+            this.MarketName = BaseCurrency + "-" + MarketCurrency;
+        }
+    }
+}
---
+++ BotCrypto/TradeResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbitrationCrypto
+{
+    public class TradeResult
+    {
+        public bool IsFilled { get; set; } //выполнен полностью или нет
+        public decimal OrigQty { get; set; } //сколько передавали на ордер
+        public decimal ExecutedQty { get; set; }  //сколько получили при выполнении ордера
+    }
+}
